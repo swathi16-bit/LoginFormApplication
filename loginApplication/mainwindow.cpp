521b8c44b43a09e01257ca65f5c1210b897cdc24
swathi16-bit <80315304+swathi16-bit@users.noreply.github.com>
2021-05-03 16:46:22 +0530

    Add files via upload

---
+++ loginApplication/mainwindow.cpp
@@ -0,0 +1,40 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include<QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+void MainWindow::on_login_clicked()
+{
+    QString username=ui->lineEdit_username->text();
+    QString password=ui->lineEdit_password->text();
+    if(username=="Test"&&password=="Test")
+       {
+           QMessageBox::information(this,"Login","Username and Password is correct");
+           loop=new Dialog(this);
+           loop->show();
+           ui->statusbar->showMessage("Username and Password is correct");
+
+       }
+       else
+       {
+            QMessageBox::warning(this,"Login","Username and Password is incorrect");
+       }
+
+}
+
+void MainWindow::on_cancel_clicked()
+{
+    close();
+}
